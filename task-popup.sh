bf33c3324911912cfa3d9ff3356a7668ed601309
Nikos Koukis <nikolaos@slamcore.com>
2018-11-19 22:50:12 +0000

    Fix bug w latest taskwarrior - Use  notation

--- task-popup.sh
+++ task-popup.sh
@@ -3,13 +3,18 @@
 # For cron: */30 * * * * DISPLAY=:0.0 /home/User/configs/TaskNotify.sh
 # Requires linux with notify-send and canberra-gtk-play which are default on Ubuntu
 
-num=`task active | wc -l`
-if [ $num -gt "1" ]
+# OWN_PATH="${BASH_SOURCE[0]}"
+# OWN_DIR="$(dirname "$OWN_PATH")"
+
+num="$(task active | wc -l)"
+if [ "$num" -gt "1" ]
   then
-    notify-send "Active Tasks" "`task active | tail -n +3 | head -n -1`" 
+    output="$(task active | tail -n +4 | head -n -1)"
+
+    notify-send -i "Active Tasks" "$output"
     canberra-gtk-play --file=/usr/share/sounds/gnome/default/alerts/drip.ogg
   else
-    notify-send "No Active Tasks" "C'mon! What are you doing?" 
+    notify-send "No Active Tasks" "C'mon! What are you doing?"
     canberra-gtk-play --file=/usr/share/sounds/ubuntu/stereo/dialog-question.ogg
 fi
 
